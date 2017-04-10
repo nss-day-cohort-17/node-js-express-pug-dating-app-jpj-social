2ac39010acea5a7d8cfed1771db58677530cb12e
krishnapriya <krishnapriyakamesh@gmail.com>
2017-04-10 14:49:41 -0500

    changed the table attributes to all lowercases

--- db/seeds/user_seed.js
+++ db/seeds/user_seed.js
@@ -4,7 +4,7 @@ const users = require('./mock_users')
 exports.seed = function(knex, Promise) {
 
   let userPromises = users.map(({username, password, first_name, last_name, gender, city, state, phone, email, photo, likes, dislikes, likedUsers}) => {
-    return knex('users').insert({userName: username, password: password, firstName: first_name, lastName: last_name, city: city, state: state, phone: phone, email: email, photo: photo, gender: gender, likedUsers: likedUsers, likes: likes, dislikes: dislikes })
+    return knex('users').insert({username: username, password: password, firstname: first_name, lastname: last_name, city: city, state: state, phone: phone, email: email, photo: photo, gender: gender, likedusers: likedUsers, likes: likes, dislikes: dislikes })
   })
   // Deletes ALL existing entries
   return knex('users').del()
