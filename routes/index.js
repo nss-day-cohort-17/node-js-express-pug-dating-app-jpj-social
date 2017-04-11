a21a1c1f5cc555f1394d56009317a891292dd7ef
krishnapriya <krishnapriyakamesh@gmail.com>
2017-04-11 14:19:48 -0500

    added login/logout route

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,18 @@
 const { Router } = require('express')
 const router = Router()
 
-
 //public routes
 
 router.use('./home-route')
-router.use('./register')
+router.use('./register-route')
+router.use(require('./login-route'))
+
+
+
+
+
+// private routes
+router.use(require('./logout-route'))
+
 
 module.exports = router;
