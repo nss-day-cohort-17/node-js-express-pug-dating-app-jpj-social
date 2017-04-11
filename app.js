f7e382b7ace5def5624c4ee33e0e5bf48d59f682
krishnapriya <krishnapriyakamesh@gmail.com>
2017-04-11 12:40:06 -0500

    added login form

--- app.js
+++ app.js
@@ -46,9 +46,11 @@ app.use((req, res, next) => {
 
 app.use(express.static('public'))
 app.use(routes)
-
+app.use((req,res)=>{
+  res.render('/login', {page: Login})
+})
 app.use((req, res) => {
-  res.render('404')
+  res.render('/')
 })
 
 
