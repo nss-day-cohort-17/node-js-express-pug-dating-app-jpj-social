deaf5219cde179b2bf939b1e39b32519ae06d55b
krishnapriya <krishnapriyakamesh@gmail.com>
2017-04-12 11:36:10 -0500

    registration is working

--- controllers/registerCtrl.js
+++ controllers/registerCtrl.js
@@ -6,21 +6,22 @@ module.exports.show = (req, res) => {
   res.render('register', { page: 'Register'});
 }
 
-module.exports.create = ({body: {userName, password, confirmation}}, res) => {
+module.exports.create = ({body: {username, password, confirmation}}, res) => {
   if (password === confirmation) {
-    User.findOneByUserName(userName)
+    console.log(username,password)
+    User.findOneByUserName(username)
     .then( (user) => {
       if (user) return res.render('register', { msg: 'user is already registered'});
-      return User.forge({userName, password})
+      return User.forge({username, password})
       .save()
       .then( () => {
         res.redirect('/profile')
       })
       // catch for save()
-      .catch( (err) => res.render('register', {msg: "Dang. There was probz. Try again."}));
+      .catch( (err) => res.render('register', {msg: `Dang1. There was probz. Try again.${err}`}));
     })
     // catch for findOneByEmail
-    .catch( (err) => res.render('register', {msg: "Dang. There was probz. Try again."}));
+    .catch( (err) => res.render('register', {msg: "Dang2. There was probz. Try again."}));
   } else {
     res.render('register', { msg: 'Oops. Password and confirmation don\'t match. Try again'});
   }
