91b0f082f95ab47d7147987c8e55259ab222f0d0
Jufebrown <jufebrown@gmail.com>
2017-04-12 21:58:50 -0500

    commented out a console.log

--- controllers/sessionCtrl.js
+++ controllers/sessionCtrl.js
@@ -8,6 +8,7 @@ module.exports.show = (req, res) => res.render('login', {page: 'Login'});
 // create method runs authentication and sends user to home if logged in
 module.exports.create = (req, res, next) =>
   passport.authenticate('local', (err, user, msg) => {
+    // console.log(user)
     if(err) return next(err)
     // when not logged in go back to login
     if(!user) return res.render('login', {page: 'Login'}, msg)
