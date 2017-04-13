8afa33e659866c38ef099a24394eaa1eb6202f63
Jufebrown <jufebrown@gmail.com>
2017-04-12 21:53:26 -0500

    removed destructuring on req object and added comment

--- controllers/registerCtrl.js
+++ controllers/registerCtrl.js
@@ -8,7 +8,13 @@ module.exports.show = (req, res) => {
   res.render('register', { page: 'Register'});
 }
 
-module.exports.create = ({body: {username, password, confirmation}}, res) => {
+// method called when POSTing on register page
+module.exports.create = (req, res) => {
+  // didn't destructure so the req object would be available down in the 2nd .then() for the redirect
+  const username = req.body.username
+  const password = req.body.password
+  const confirmation = req.body.confirmation
+  // checks to see if password and password confirmation are the same
   if (password === confirmation) {
     User.findOneByUserName(username)
     .then( (user) => {
