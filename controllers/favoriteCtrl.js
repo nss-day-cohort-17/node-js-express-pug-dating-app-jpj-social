1c8c3e705bc31f2b34be1ea9315bc32561ca965c
Jufebrown <jufebrown@gmail.com>
2017-04-14 10:55:28 -0500

    changed indentation

--- controllers/favoriteCtrl.js
+++ controllers/favoriteCtrl.js
@@ -7,16 +7,15 @@ const User = require('../models/userModel')
 module.exports.show = (req, res, next) => {
   // console.log("sessions.username",username);
   User.getAllLikedUsers(req.session.user)
-    .then((users) => {
-      console.log("users",users[0].likedusers);
-      let userPromise = users[0].likedusers
-      userPromise = userPromise.map(id =>{
-        return User.forge({id : id}).fetch()
-      })
-      Promise.all(userPromise)
-      .then ((likedUserArr) =>{
-        res.render('favorite', {page: 'Favorites', likedUserArr})
-      })
-
+  .then((users) => {
+    // console.log("users",users[0].likedusers);
+    let userPromise = users[0].likedusers
+    userPromise = userPromise.map(id =>{
+      return User.forge({id : id}).fetch()
     })
+    Promise.all(userPromise)
+    .then ((likedUserArr) =>{
+      res.render('favorite', {page: 'Favorites', likedUserArr})
+    })
+  })
 }
